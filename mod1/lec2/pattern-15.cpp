c918bb76da4f82d43f014b76f3a0a91d5c738860
Sohan Dutta <sohan.dutta11@gmail.com>
2025-05-02 19:57:07 +0530

    feat: lecture 2 pattern 15

---
+++ mod1/lec2/pattern-15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * For N = 5
+ * Pattern:
+ *
+ *  TC - O(N*2)
+ */
+int main() {
+  cout << "Enter the value for N: ";
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << char(65+j);
+    }
+    cout << endl;
+  }
+}
