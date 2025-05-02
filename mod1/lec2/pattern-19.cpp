71d5545e0c435e9b9297e8736d5c24f9ec3b3c42
Sohan Dutta <sohan.dutta11@gmail.com>
2025-05-02 23:26:00 +0530

    feat: lecture 2 pattern 19

---
+++ mod1/lec2/pattern-19.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * For N = 3
+ * Pattern:
+ * ******
+ * **  **
+ * *    *
+ * **  **
+ * ******
+ *
+ *  TC - O(N*2)
+ */
+int main() {
+  cout << "Enter the value for N: ";
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n*2; i++) {
+    if (i < n) {
+      for (int j = 0; j < n - i; j++) {
+        cout << "*";
+      }
+      
+      for (int j = 0; j < 2*i; j++) {
+        cout << " ";
+      }
+
+      for (int j = 0; j < n - i; j++) {
+        cout << "*";
+      }
+    } else {
+      for (int j = 0; j < i - n + 1; j++) {
+        cout << "*";
+      }
+
+      for (int j = 0; j < 2*(2*n - i - 1); j++) {
+        cout << " ";
+      }
+
+      for (int j = 0; j < i - n + 1; j++) {
+        cout << "*";
+      }
+    }
+    cout << endl;
+  }
+}
