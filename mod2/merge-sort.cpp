fc1a906af104e8d33a0fa4ace9a255fa37303afe
Sohan Dutta <sohan.dutta11@gmail.com>
2025-05-13 20:20:08 +0530

    feat: module 2, merge sort

---
+++ mod2/merge-sort.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity = O(log2 N)
+void merge(vector<int> &arr, int low, int mid, int high) {
+  int left = low;
+  int right = mid + 1;
+  vector<int> temp;
+
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+       temp.push_back(arr[left]);
+       left++;
+    } else {
+      temp.push_back(arr[right]);
+      right++;
+    }
+  }
+
+  while (left <= mid) {
+    temp.push_back(arr[left]);
+    left++;
+  }
+
+  while (right <= high) {
+    temp.push_back(arr[right]);
+    right++;
+  }
+
+  for (int i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+  if (low == high) return;
+
+  int mid = (low + high) / 2;
+
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+
+  merge(arr, low, mid, high);
+}
+
+int main() {
+  vector<int> arr = {9, 12, 14, 15, 6, 8, 13};
+
+  for (auto it: arr) cout << it << ", ";
+  cout << endl;
+
+  mergeSort(arr, 0, arr.size() - 1);
+
+  for (auto it: arr) cout << it << ", ";
+  cout << endl;
+  return 0;
+}
